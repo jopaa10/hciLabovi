484d670cc298bc519317f00facccafac144652a4
jopaa10 <jopamate@gmail.com>
2020-10-22 10:51:35 +0200

    Adding Logo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 import ContactBar from '../components/contactBar'
-import NavigationHeader from '../components/NavigationHeader'
+import NavigationHeader from '../components/contactBar/NavigationHeader'
 
 const IndexPage = () => (
   <main>
